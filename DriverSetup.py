e471ac90b9650034ea35e14a821beee8f142e44f
Stan <78124828+StanChung624@users.noreply.github.com>
2022-05-25 09:04:44 +0800

    Update DriverSetup.py

--- DriverSetup.py
+++ DriverSetup.py
@@ -4,8 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from time import sleep
-
-from setuptools import find_namespace_packages
+from StringProcess import setw
 
 #### importing class "By" and "Select" ####
 class DriverUtility():
@@ -24,7 +23,7 @@ class DriverSetup(Chrome):
             ::
                 driver = DriverSetup()
 
-        :rtype: WebElement
+        :rtype: Driver
     """
     def __init__(self):
         options = Options()
@@ -32,31 +31,15 @@ class DriverSetup(Chrome):
         self = super().__init__(options=options)
 
     def endDriver(self):
+        end_msg = 'Driver end in 5 second...'
+        for i in range(5,0,-1):
+            print('Driver end in '+str(i)+' second...'+'\r'*len(end_msg),end='')
+            sleep(1)
+        print('')
         print('*'*51)
-        print('** Auto fill-in completed. Press Enter to Exit...**')    
-        input('*'*51+'\n')
+        print(setw('* Auto fill-in completed. *',51,'mid'))
+        print('*'*51+'\n')
 
-    def monthDays(self, startYYYmm):
-        startYYY = startYYYmm[:3]
-        startmm = startYYYmm[3:]
-        if startYYY in ['113','117','121']:
-            days = {['01','03','05','07','08','10','12']:'31',
-                    ['04','06','09','11']:'30',
-                    '02':'29'}
-        else:
-            days = {['01','03','05','07','08','10','12']:'31',
-                    ['04','06','09','11']:'30',
-                    '02':'28'}
-        if int(startmm) == 12:
-            endYYY = str(int(startYYY)+1)
-            endmm = '01'
-        else:
-            endYYY = startYYY
-            endmm = str(int(startmm)) + 1
-        if len(endmm) == 1:
-            endmm = '0' + endmm
-
-        return [endYYY, endmm, days[endmm]]
 
     def send_ScrollDown(self, times:int=1):
         for i in range(times):
@@ -71,36 +54,37 @@ class DriverSetup(Chrome):
         body = self.find_element_by_css_selector('body')
         body.send_keys(Keys.ENTER)
 
-    def loop_for_element_click(self, by:By, value:str, sleep_time:float = 0.5, loop_max:int = 10):
+    def find_element_then_click(self, by:By, value:str, sleep_time:float = 0.5, loop_max:int = 10):
         loop_index = 0
         looper = True
+        # attemp-session
+        try:
+            self.find_element(by=by, value=value).click()
+            looper = False
+        except:
+            try:
+                sleep(0.5)
+                element = self.find_element(by=by, value=value)
+                self.execute_script("arguments[0].click();", element)
+                looper = False
+            except:
+                pass        
+        # loop-session
         while loop_index != loop_max and looper:
             loop_index += 1
             if loop_index >= 2:
                 self.send_ScrollDown()
             try:
-                self.find_element(by=by, value=value).click()
+                element = self.find_element(by=by, value=value)
+                self.execute_script("arguments[0].click();", element)
                 looper = False
             except:
                 sleep(sleep_time)
                 self.send_ScrollDown()
-                self.find_element_then_click(by=by, value=value)
+                self.find_element(by=by, value=value).click()
                 looper = False
             finally:
                 continue
 
         if looper:
             self.find_element(by=by, value=value).click()
-
-    def find_element_then_click(self, by:By, value:str):
-        try:
-            element = self.find_element(by=by, value=value)
-        except:
-            sleep(0.5)
-            element = self.find_element(by=by, value=value)
-        
-        try:
-            element.click()
-        except:
-            sleep(0.5)
-            element.click()
